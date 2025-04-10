e8636dc4f77156b5fbe9baecbaacd33c9d22737f
NoelThoujanba <mrjamba269@gmail.com>
2025-04-10 15:59:55 +0530

    added call.c

---
+++ call.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void CallByValue(int n)
+{
+    printf("Value of n passed to CallByValue(n) is %d\n", n);
+    n += 1;
+    printf("Added +1 to n\n");
+}
+
+void CallByReference(int* n)
+{
+    if(n == NULL)
+    {
+        printf("Value of n passed to CallByReference(n) is NULL. Exiting funtion\n");
+        return;
+    }
+    printf("Value of n passed to CallByReference(n) is %d", *n);
+    *n += 1;
+
+    printf("Added +1 to n\n");
+}
+
+int main(void)
+{
+    int n;
+    printf("n: ");
+    scanf("%d", &n);
+
+    printf("Call By Value\n");
+    CallByValue(n);
+    printf("Value of n in main() after function is called: %d\n", n);
+
+    printf("Call By Reference\n");
+    CallByReference(&n);
+    printf("Value of n in main() after function is called: %d\n", n);
+}
